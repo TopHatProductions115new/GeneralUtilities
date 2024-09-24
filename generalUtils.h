f20d2bbfe0c48c3a5881b9deb5156ad15abf42e1
TopHatProductions115 <114023815+TopHatProductions115new@users.noreply.github.com>
2024-09-23 22:46:01 -0400

    Create generalUtils.h
    First header file added. Starting small.

---
+++ generalUtils.h
@@ -0,0 +1,14 @@
+# ifndef GENERALUTILS_H
+# define GENERALUTILS_H
+
+// generalUtils.h
+
+
+typedef unsigned int bool;
+enum { false, true };
+
+void setTrue(bool value);
+void setFalse(bool value);
+void printBool(bool value, bool newline);
+
+# endif
