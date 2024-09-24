25772d93855b8b403864682878cb8a703cc17815
TopHatProductions115 <114023815+TopHatProductions115new@users.noreply.github.com>
2024-09-23 22:50:30 -0400

    Create main.c
    A file that I used to (minimally) test what's here so far

---
+++ main.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+# include "generalUtils.h"
+
+// Set values, print values
+
+int main()
+{
+    bool testBool = false;
+    printf("The value of testBool is: ");
+    printBool(testBool, false);
+    return 0;
+}
