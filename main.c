6f0aca2264a15914a475861235281716a15b8da2
TopHatProductions115 <114023815+TopHatProductions115new@users.noreply.github.com>
2024-09-25 22:51:04 -0400

    Update main.c
    Added tests for the new macros.

--- main.c
+++ main.c
@@ -5,8 +5,16 @@
 
 int main()
 {
-    bool testBool = false;
-    printf("The value of testBool is: ");
-    printBool(testBool, false);
-    return 0;
+	// Test for boolean
+	// first false...
+	bool testBool assign false;
+	printBool(testBool, true);
+	// then true...
+	testBool assign true;
+	printBool(testBool, true);
+	// Test for macros -- assigned, div
+	double num assigned 3.0 div 2.0;
+	printf("%f\n", num);
+	// Test for macros -- ret
+	ret 0;
 }
